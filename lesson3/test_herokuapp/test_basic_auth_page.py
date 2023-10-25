1ef09a47e7982b86360cfe799516a232d400728b
ZhenyaZhadko <zhadko.evgenia@gmail.com>
2023-10-25 22:32:39 +0300

    added changes to test_basic_auth_page.py in lesson3

--- lesson3/test_herokuapp/test_basic_auth_page.py
+++ lesson3/test_herokuapp/test_basic_auth_page.py
@@ -5,5 +5,6 @@ from lesson3.test_herokuapp.locators import TITLE_BASIC_AUTH_PAGE, TEXT_SUCCESSF
 
 def test_basic_auth_with_correct_credentials(driver):
     driver.get(BASIC_AUTH_PAGE_WITH_CORRECT_CREDENTIALS)
+    text_success_auth = driver.find_element(By.XPATH, TEXT_SUCCESSFUL_AUTH).text
     assert driver.find_element(By.XPATH, TITLE_BASIC_AUTH_PAGE).text == 'Basic Auth'
-    assert driver.find_element(By.XPATH, TEXT_SUCCESSFUL_AUTH).text == ('Congratulations! You must have the proper credentials.')
+    assert text_success_auth == 'Congratulations! You must have the proper credentials.'
